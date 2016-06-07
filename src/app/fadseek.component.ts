8c862f053a62eeeed84e69e2ce2efd30ec467049
sea <sea@seahat.home>
2016-06-07 09:21:22 -0700

    test commit

--- src/app/fadseek.component.ts
+++ src/app/fadseek.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class FadseekAppComponent {
   title = 'fadseek works!';
+  i = 0;
+  onClick($event){
+    console.log("onClick", $event);
+    this.i++;
+    this.title = "you have clicked this " + this.i +" times";
+  }
 }
